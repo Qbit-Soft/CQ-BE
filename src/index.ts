ea68c5c1d81a1cfcbe482670289b13a83977affb
Edwin-0396 <egc4611@gmail.com>
2024-08-22 18:47:40 -0500

    new

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import path from 'path';
 import formidable, { File, Files, IncomingForm } from 'formidable';
 import fs from 'fs';
-import userRoutes from './routes/userRoutes';  // Asegúrate de ajustar la ruta según la estructura de tu proyecto
+import userRoutes from './routes/userRoutes';  // Rutas de usuario
+import orderRoutes from './routes/orderRoutes';  // Rutas de orden de compra
 
 const app = express();
 const outputPath = path.join(__dirname, 'outputFiles');
@@ -12,18 +13,19 @@ app.use(express.json());
 
 async function startServer() {
     try {
+        // Crear directorio de salida si no existe
         if (!fs.existsSync(outputPath)) {
             fs.mkdirSync(outputPath);
         }
 
-        // Ruta para subir archivos (como ya la tienes definida)
-        app.post('/api/test-hse-upload', (req, res) => {
+        // Ruta para subir archivos HSE
+        app.post('/api/test-hse-upload', (req: Request, res: Response) => {
             const form = new IncomingForm({
                 uploadDir: outputPath,
                 keepExtensions: true,
             });
 
-            form.parse(req, (err, fields, files: Files) => {
+            form.parse(req, (err: any, fields: formidable.Fields, files: Files) => {
                 if (err) {
                     console.error('Error parsing the form:', err);
                     res.status(500).send('Error parsing the form');
@@ -50,7 +52,10 @@ async function startServer() {
         });
 
         // Montar las rutas de usuarios
-        app.use('/api', userRoutes);  // Prefijo '/api' para las rutas de usuario
+        app.use('/api/users', userRoutes);  // Prefijo '/api/users' para las rutas de usuario
+
+        // Montar las rutas de orden de compra
+        app.use('/api/orders', orderRoutes);  // Prefijo '/api/orders' para las rutas de orden de compra
 
         const PORT = process.env.PORT || 3000;
         app.listen(PORT, () => {
